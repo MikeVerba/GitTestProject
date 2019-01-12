88a064ba44b0585396d75cb3c7c644e7968b7134
MikeVerba <michal.wierba@gmail.com>
2019-01-12 11:24:57 +0100

    added goodbye method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git rocks!!");
         System.out.println("First change!!");
+        welcome();
+        goodbye();
 
     }
 
     public static void welcome(){
         System.out.println("Welcome!");
+    }
 
+    public static void goodbye(){
+        System.out.println("Goodbye!");
     }
 }
