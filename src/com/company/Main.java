62c76d260ee1375d2975b5d8bb68d7ff7086b5cc
MikeVerba <michal.wierba@gmail.com>
2019-01-12 11:08:25 +0100

    added new method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git rocks!!");
         System.out.println("First change!!");
+
+    }
+
+    public static void welcome(){
+        System.out.println("Welcome!");
+
     }
 }
